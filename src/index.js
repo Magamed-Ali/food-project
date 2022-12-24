68ae9224f63ab85802e08380ecfcc282cb66de98
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-24 20:45:03 +0300

    delete test

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
-import Test from "./Test";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-          <Test />
+          <App />
       </BrowserRouter>
   </React.StrictMode>
 );
