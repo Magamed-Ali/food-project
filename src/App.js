6ddfb921469861344dbb77be12b218feb6bfc656
MuhammadAli <magamedali93@mail.ru>
2023-08-08 01:51:28 +0300

    fixed App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from "./Layout/Header";
 import Main from "./Layout/Main";
 import Footer from "./Layout/Footer";
@@ -9,9 +8,7 @@ import Home from "./components/Home";
 import {Category} from "./Layout/Category";
 import Recipe from "./components/Recipe";
 
-
 function App() {
-
     function powersOfTwo(n){
 
         let arr = [];
@@ -23,21 +20,17 @@ function App() {
         return arr
     }
 
-    console.log(powersOfTwo(4))
-
   return (
     <div className="App">
      <Header/>
         <main className="container content ">
             <Routes>
                 <Route path="/" element={<Home/> }/>
-
                 <Route path="/" element={<Main/> }/>
                 <Route path="/about" element={<About/> }/>
                 <Route path="/contacts" element={<Contact/> }/>
                 <Route path="/category/:name" element={<Category/> }/>
                 <Route path="/meal/:id" element={<Recipe/>}/>
-
             </Routes>
         </main>
       <Footer/>
