1bc1ab6a22ba4afd4e3ad0c95bf30a499ea95305
MuhammadAli <magamedali93@mail.ru>
2023-08-09 20:31:34 +0300

    fixed config

--- src/Layout/Category.js
+++ src/Layout/Category.js
@@ -1,14 +1,12 @@
 import {useEffect, useState} from "react";
 import {useParams, useNavigate} from 'react-router-dom';
 import {getFilteredCategory} from '../api';
-
 import {MealList} from "../components/MealList";
 import Preloader from "../components/Preloader";
 
 function Category() {
     const {name} = useParams();
     const [meals, setMeals] = useState([]);
-
     const navigate = useNavigate();
 
     function handleClick() {
--- src/api.js
+++ src/api.js
@@ -4,12 +4,10 @@ const getMealById = async (mealID) => {
    const response = await fetch(API_URL + 'lookup.php?i=' + mealID);
    return await response.json();
 }
-
 const getAllCategories = async () => {
    const response = await  fetch(API_URL + 'categories.php');
    return await response.json();
 }
-
 const getFilteredCategory = async (catName) => {
    const response = await fetch(API_URL + 'filter.php?c=' + catName);
    return await response.json();
